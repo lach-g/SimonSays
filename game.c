1c4593c5bce8a33d466adf24ff4091f410c36647
Lachlan Greenland <lachlan.greenland@gmail.com>
2020-11-30 21:09:09 +0800

    simple dance function made

--- game.c
+++ game.c
@@ -8,10 +8,12 @@ int main()
     int i;
     for(i = 0; i < STICK_REPEAT; i++)
     {
+        printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
         print_stick_man();
-        printf("\n\n\n\n\n\n\n\n\n");
         sleep(1);
+        printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
         print_stick_man_wave();
+        sleep(1);
     }
     return 0;
 }
