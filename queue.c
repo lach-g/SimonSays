5b506b72ad7feac5f1d802b5e32f48b6d9651e77
Lachlan Greenland <lachlan.greenland@gmail.com>
2020-12-27 12:18:19 +0800

    fixed the command description comparisons so they are now playing correct actions

--- queue.c
+++ queue.c
@@ -1,4 +1,5 @@
 #include <string.h>
+#include <stdio.h>
 #include "queue.h"
 
 void enqueue(list_t* list, void* item_adding)
@@ -40,21 +41,34 @@ command_t* create_command_action(command_t* to_complete)
     strcpy(trick_command, to_complete->description);
     trick_command[16] = '\0';
 
+    char cut_input[COM_STRING_LEN];
+    strcpy(cut_input, to_complete->description);
+    cut_input[16] = '\0';
+
     list_t* command_refs = create_command_reference_list();
 
+    int found;
     for(list_node_t* current = command_refs->head; current != NULL; current = current->next)
     {
-       int found = 0;
+       found = 0;
        command_t* command_ref = current->data;
+       printf("to complete: %s compared to: %s\n", to_complete->description, command_ref->description);
        if(strcmp(to_complete->description, command_ref->description) == 0)
        {
            to_complete->action = command_ref->action;
            found = 1;
+           break;
        }
-       if(!found)
+       else if (strcmp(cut_input, trick_command) == 0)
        {
-           to_complete->action = &shrug;
+           to_complete->action = &shake_head;
+           found = 1;
+           break;
        }
+    }
+    if(!found)
+    {
+        to_complete->action = &shrug;
     }
     return to_complete;
 }
@@ -67,13 +81,13 @@ list_t* create_command_reference_list()
     command_t* dance_ref = match_description_action("Simon says dance", &dance);
     enqueue(all_descriptions, dance_ref);
 
-    command_t* left_wave_ref = match_description_action("Simon says left wave", &left_wave);
+    command_t* left_wave_ref = match_description_action("Simon says left hand up", &left_wave);
     enqueue(all_descriptions, left_wave_ref);
 
-    command_t* right_wave_ref = match_description_action("Simon says right wave", &right_wave);
+    command_t* right_wave_ref = match_description_action("Simon says right hand up", &right_wave);
     enqueue(all_descriptions, right_wave_ref);
 
-    command_t* touch_head_ref = match_description_action("Simon says touch head", &touch_head);
+    command_t* touch_head_ref = match_description_action("Simon says hands on head", &touch_head);
     enqueue(all_descriptions, touch_head_ref);
 
     command_t* didnt_say_ref = match_description_action("Simon didn't say", &shake_head);
